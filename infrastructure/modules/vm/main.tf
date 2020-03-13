2e169cd28240c03f55038f513dc83b0d2203f872
Viktor Braun (v_b) <mail@viktor-braun.de>
2020-03-13 12:14:58 +0100

    add custom generated private key

--- infrastructure/modules/vm/main.tf
+++ infrastructure/modules/vm/main.tf
@@ -1,25 +1,31 @@
+locals {
+  key_path = "${path.module}/../provisioning/keys"
+  authorized_keys = fileset(local.key_path, "*.pub")
+} 
+
+
 resource "azurerm_public_ip" "ip" {
-    name                         = "${var.name}-ip"
-    location                     = var.destination.location
-    resource_group_name          = var.destination.resource_group_name
-    allocation_method            = "Static"
-    domain_name_label            = var.name
-    tags = var.tags
+  name                         = "${var.name}-ip"
+  location                     = var.destination.location
+  resource_group_name          = var.destination.resource_group_name
+  allocation_method            = "Static"
+  domain_name_label            = var.name
+  tags = var.tags
 }
 
 resource "azurerm_network_interface" "ni" {
-    name                         = "${var.name}-ni"
-    location                     = var.destination.location
-    resource_group_name          = var.destination.resource_group_name
+  name                         = "${var.name}-ni"
+  location                     = var.destination.location
+  resource_group_name          = var.destination.resource_group_name
 
-    ip_configuration {
-        name                          = "${var.name}-ni-config"
-        subnet_id                     = var.destination.subnet_id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id          = azurerm_public_ip.ip.id
-    }
+  ip_configuration {
+    name                          = "${var.name}-ni-config"
+    subnet_id                     = var.destination.subnet_id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.ip.id
+  }
 
-    tags = var.tags
+  tags = var.tags
 }
 
 resource "azurerm_virtual_machine" "vm" {
@@ -30,36 +36,40 @@ resource "azurerm_virtual_machine" "vm" {
   vm_size               = var.size
 
   storage_os_disk {
-      name              = "${var.name}-disc"
-      caching           = "ReadWrite"
-      create_option     = "FromImage"
-      managed_disk_type = "Premium_LRS"
+    name              = "${var.name}-disc"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Premium_LRS"
   }
 
   storage_image_reference {
-      publisher = "Canonical"
-      offer     = "UbuntuServer"
-      sku       = "18.04-LTS"
-      version   = "latest"
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
   }
 
   os_profile {
-      computer_name  = var.name
-      admin_username = var.auth.username
-      custom_data = var.provisioning_script
+    computer_name  = var.name
+    admin_username = var.auth.username
+    custom_data = var.provisioning_script
   }
 
   os_profile_linux_config {
-      disable_password_authentication = true
-      ssh_keys {
-          path     = "/home/${var.auth.username}/.ssh/authorized_keys"
-          key_data = var.auth.pubkey
+    disable_password_authentication = true
+    
+    dynamic "ssh_keys" {
+      for_each = local.authorized_keys
+      content {
+          key_data = file("${local.key_path}/${ssh_keys.value}")
+          path = "/home/${var.auth.username}/.ssh/authorized_keys"
       }
+    }
   }
 
   boot_diagnostics {
-      enabled = "true"
-      storage_uri = var.diag_storage_endpoint
+    enabled = "true"
+    storage_uri = var.diag_storage_endpoint
   }
 
   tags = var.tags
